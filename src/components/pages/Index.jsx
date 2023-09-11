ca7468df6ddaf9f4b57261d975a8a01fe93712a0
Jesus Roman <jesus_fime@outlook.es>
2023-09-11 13:00:22 -0600

    exportando funcion de loader en Index hacia main

--- src/components/pages/Index.jsx
+++ src/components/pages/Index.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+//Esta funcion loader se va a ejecutar cuando el componente cargue, es similar al useEffect
+//Nota: ESTE LOADER SIEMPRE TIENE QUE RETORNAR ALGO
+//Nota: este se tiene que importar en el componente DONDE FUE DELCARADO, en este caso el Index.jsx fue declarado en el MAIN.jsx
+//Nota: al importarlo debemos cambiarle el nombre ej. loader as nuevoNombre, la razon de porque aqui lo declaramos como loader
+//y no con otro nombre es que asi espera ese nombre react-router-dom
+//Nota: despues ya lo especificamos en nuestra ruta
+export function loader(){
+
+  return 'Desde Loader';
+}
+
 
 function index() {
   return (
-    <div>Clientesssssss</div>
+    <div>
+      <h1 className='font-black text-4xl text-blue-900'>Clientes</h1>
+      <p className='mt-3'>Administra tus Clientes</p>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import './index.css'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import Layout from './components/Layout'
 import NuevoCliente from './components/pages/NuevoCliente'
-import Index from './components/pages/Index'
+//Nota al importar el loader, de preferencia siempre guardarlo con un nuevo nombre
+import Index, {loader as clientesLoader} from './components/pages/Index'
 
 const router = createBrowserRouter([ //createBrowserRouter toma un array de objetos donde pondremos nuestras rutas, este se lo pasamos al Provider
   {
@@ -13,7 +14,8 @@ const router = createBrowserRouter([ //createBrowserRouter toma un array de obje
     children: [ /* todo lo que este dentro de children tendra aplicado el Layout (elemento) de arriba */
       {
         index: true, /* aqui no se le define un path porque este "children" se va a cargar cuando visitemos la pagina principal, es y no es un children a la vez */
-        element: <Index />
+        element: <Index />,
+        loader: clientesLoader
       },
       {
           path: '/clientes/nuevo',
